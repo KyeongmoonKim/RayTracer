5a8357e06034c9d9329920c2ab0d3efc5a047568
KyeongmoonKim <kkm8031@snu.ac.kr>
2019-05-30 10:24:02 -0700

    finding error...

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -22,12 +22,12 @@
 using namespace Magick;
 using namespace std;
 
-double refP[] = {10.0, 250.0, 375.0}; //when refP is away from window, zoom : in, reverse is zoom out
-double windowCenter[] = {10.0, 60.0, 90.0};
+double refP[] = {0.0, 0.0, 200.0}; //when refP is away from window, zoom : in, reverse is zoom out
+double windowCenter[] = {0.0, 0.0, 100.0};
 double pixels[H][W][3];
 double* pixelD(int row, int col);
 double color(double* o, double* v, double* rgb, double dist, int bType, int bIdx); //o : reference point, v : vector, return : rgb vector
-void setObject(string str); //str is file name
+void setMyObject(); //str is file name
 double calculD(double* vertex, double* normal); //normal is normal vector of plane, vertex is on the plane.
 int parallel(double* v1, double* v2);
 	
@@ -74,6 +74,7 @@ double interPlane(double* o, double* u, Plane* p); //the same as interSphere.
 int crossVect(double* pInter, double* u, double* p0, double* p1, double* normal); //return value : 0(no cross), 1(cross exists).
 //crossVect is function for inside-outside test. find crosspoints exists with edge between p0 and p1, and half-line u
 void setTexture(string str, int a, int b, int c, int d);
+void objParser(string str, double* trans, double scale);
 double* sphereTexture(int idx, double* normal);
 double* planeTexture(int idx, double* point);
 double* calculT(double *L, double* N, double ni, double nr);
@@ -82,21 +83,39 @@ double innerSphere(double* o, double* u, Sphere* s);
 Light* lights;
 Sphere* spheres;
 Plane* planes;
+
+double** vertexs;
+double** normals;
+
 int lightNum;
 int sphereNum;
 int planeNum;
 
+//objfile byeonsu
+int vertexNum;
+int normalNum; 
+
+int planesSize = 256;
+int spheresSize = 256;
+
+int vertexsSize = 256;
+int normalsSize = 256;
 //for debugging
 int currRow;
 int currCol;
 
 int main(int argc, char** argv) {
 	InitializeMagick(*argv);
-	setObject("test");
+	setMyObject();
 	string tfName;
+	cout<<"please enter your obj file : ";
+	string objName;
+	cin>>objName;
+	double transV[] = {10.0, 0.0, 50.0};
+	objParser(objName, transV, 20.0);
 	cout<<"please enter your texure information file : ";
 	cin>>tfName;
-	ifstream input2("texture.txt");
+	ifstream input2(tfName.data());
 	string line;
 	getline(input2, line);
 	int textureNum = stoi(line);
@@ -120,9 +139,11 @@ int main(int argc, char** argv) {
 	}
 	for(int i = 0; i < H; i++) {
 		for(int j = 0; j < W; j++) {
+			//cout<<i << " " <<j<<endl;
 			currRow = i;
 			currCol = j;
 			double* v = pixelD(i, j);
+			//cout<<v[0]<<", "<<v[1]<<", "<<v[2]<<endl;
 			double* rgb = (double *)malloc(sizeof(double)*3);
 			double ret = color(refP, v, rgb, 0.0, -1, -1);
 			if(ret < 0.0) { //background color is black.
@@ -203,23 +224,25 @@ double* pixelD(int row, int col) {
 } //normal vector for reference point to pixel.
 
 double color(double* o, double* v, double* rgb, double dist, int bType, int bIdx) {
-	if(dist > 5000.0) return -1.0; //so far away.
+	if(dist > 2000.0) return -1.0; //so far away.
 	double s = 20000.0;
 	int currIdx;
 	int type = 0;//0 is sphere, 1 is polygon.
 	for(int i = 0; i < sphereNum; i++) {
 		if(bType==0 && bIdx == i) continue; //pass the reflexed surface
 		double tempS = interSphere(o, v, &(spheres[i]));
-		if(tempS < s && tempS > 0.1) {//when the point is common point, regard as the same point
+		if(tempS < s && tempS > 1) {//when the point is common point, regard as the same point
 			s = tempS; 
 			currIdx = i;
 		}
 	}
-	
+	if(currRow==0&&currCol==146) cout<<"start"<<endl;
 	for(int i = 0; i < planeNum; i++) {
+		if(currRow==0&&currCol==146) cout<<"i in color : "<<i<<endl;
 		if(bType==1 && bIdx == i) continue; //pass the reflexed surface
 		double tempS = interPlane(o, v, &(planes[i]));
-		if(tempS < s && tempS > 0.1) {
+		if(tempS < s && tempS > 1) {
+			if(currRow==0&&currCol==146&&i>5) cout<<"###############################################################################33"<<endl;
 			s = tempS;
 			currIdx = i;
 			type = 1;
@@ -317,7 +340,7 @@ double color(double* o, double* v, double* rgb, double dist, int bType, int bIdx
 	
 	//reflection part.
 	//if(currRow==211&&currCol==269) cout<<"before reflecton : "<< pInter[0] <<", "<<pInter[1]<<", "<<pInter[2]<<endl;
-	double* reflRgb = (double*)malloc(sizeof(double)*3);
+	/*double* reflRgb = (double*)malloc(sizeof(double)*3);
 	double vNew[3];
 	for(int i = 0; i < 3; i++) vNew[i] = 2.0 * dotProduct(N, V) * N[i] - V[i]; //reflected v vector.
 	double reflS = color(pInter, vNew, reflRgb, dist+s, type, currIdx); //reflRgb is light from vNew.
@@ -377,7 +400,7 @@ double color(double* o, double* v, double* rgb, double dist, int bType, int bIdx
 		}
 		free(T);
 		free(refrRgb);
-	}
+	}*/
 	for(int i = 0; i < 3; i++) rgb[i] = tempRgb[i];
 	return s;
 }
@@ -400,10 +423,10 @@ double innerSphere(double* o, double *u, Sphere *s) {
 	double b = -2.0 * dotProduct(u, deltaP);
 	double c = length(deltaP, 3) * length(deltaP, 3) - (s->r) * (s->r);
 	double temp = b*b - 4.0 * c;
-	if(temp < -0.000001) {//no intersecton.
+	if(temp < 0.0) {//no intersecton. original : -0.00001
 		cout << "inner sphere no intersection, it's bug!" << endl;
 		return 20000.0;
-	} else if(temp > 0.000001) {//2 pont intersection.
+	} else if(temp > 0.0) {//2 pont intersection. original : 0.00001
 		double s1 = (-1.0 * b - sqrt(temp)) / 2.0;
 		double s2 = (-1.0 * b + sqrt(temp)) / 2.0;
 		double ret = max(s1, s2);
@@ -423,9 +446,9 @@ double interSphere(double* o, double* u, Sphere* s) {
 	double b = -2.0 * dotProduct(u, deltaP);
 	double c = length(deltaP, 3) * length(deltaP, 3) - (s->r) * (s->r);
 	double temp = b*b - 4.0 * c;
-	if(temp < -0.0000001) { // no intersection.
+	if(temp < 0.0) { // no intersection. origianl : -0.0000001
 		return 20000.0;
-	} else if(temp > 0.000001) { //2 point intersection
+	} else if(temp > 0.0) { //2 point intersection origianl : 0.0000001
 		double s1 = (-1.0 * b - sqrt(temp)) / 2.0;
 		double s2 = (-1.0 * b + sqrt(temp)) / 2.0;
 		/*double ret = min(s1, s2);
@@ -444,31 +467,47 @@ double interSphere(double* o, double* u, Sphere* s) {
 }//caculate intersect point Pi. Let, point Pi = o + s*u, return value is s. hyphothesis : o is out of the sphere.
 
 double interPlane(double* o, double* u, Plane* p) {
+	if(currRow==0&&currCol==146) cout<<currRow<<", "<<currCol<<endl;
+	if(currRow==0&&currCol==146) cout<<"interPlane"<<endl;
+	if(currRow==0&&currCol==146) cout<<"plane"<<endl;
+	for(int i = 0; i < p->n; i++) {
+		if(currRow==0&&currCol==146) cout<<"vertex "<<i<<" : "<<p->vertex[i][0]<<", "<<p->vertex[i][1]<<", "<<p->vertex[i][2]<<endl;
+	}
+	if(currRow==0&&currCol==146) cout<<"o : "<< o[0]<<", "<<o[1]<<", "<<o[2]<<endl;
+	if(currRow==0&&currCol==146) cout<<"u : "<< u[0]<<", "<<u[1]<<", "<<u[2]<<endl;
 	double s;
-	if(dotProduct(p->normal, u) < 0.00001 && dotProduct(p->normal, u) > -0.00001) {//on plane or parallel to plane
-		double temp = dotProduct(p->normal, o) + p->D;
-		if(temp < 0.000001 && temp > -0.000001) s = 0.0;
-		else return 20000.0; //no intersection
+	if(dotProduct(p->normal, u) < 0.001 && dotProduct(p->normal, u) > -0.001) {//on plane or parrallel to plane
+		return 20000.0;
+		/*double temp = dotProduct(p->normal, o) + p->D;
+		if(temp == 0.0) s = 0.0;
+		//if(temp < 0.000001 && temp > -0.000001) s = 0.0;
+		else return 20000.0; //no intersection*/
 	} else {
 		s = -1.0*(p->D + dotProduct(p->normal, o)) / dotProduct(p->normal, u);
 	}
 	if(s < 0.0) {
 		return 20000.0; //no intersection.
 	}
+	if(currRow==0&&currCol==146) cout<<"s : "<<s<<endl;
 	double pInter[3];
 	for(int i = 0; i < 3; i++) pInter[i] = o[i] + s * u[i];
 	
 	//inside-outside test.
+	//int time = 0;
 	int crossNum = 0;
 	double uPlane[3];
+	if(currRow==0&&currCol==146) cout<<"middle point 0 1 : ";
 	for(int i = 0; i < 3; i++) {
-		uPlane[i] = (p->vertex[1][i] + p->vertex[0][i])/2;
-		uPlane[i] -= pInter[i];
+		if(currRow==0&&currCol==146) cout<<(p->vertex[1][i] + p->vertex[0][i])/2 <<" ";
+		uPlane[i] = (p->vertex[1][i] + p->vertex[0][i])/2 - pInter[i];
 	}//uPlane : pInter -> mid(vertex[0], vertex[1]) vector.
+	if(currRow==0&&currCol==146) cout<<endl;
 	double d = length(uPlane, 3);
 	for(int i = 0; i < 3; i++) uPlane[i] = uPlane[i]/d;
 	for(int i = 0; i < p->n; i++) {//find existence of cross point.
 		crossNum += crossVect(pInter, uPlane, p->vertex[i], p->vertex[(i+1)%(p->n)], p->normal);
+		if(currRow==0&&currCol==146) cout<<"i : "<<i<<endl;
+		if(currRow==0&&currCol==146) cout<<"crossNum : "<< crossNum<<endl;
 	}
 	if(crossNum%2==0) {//even is outside. so intersection doesn't exist.
 		return 20000.0;
@@ -478,30 +517,42 @@ double interPlane(double* o, double* u, Plane* p) {
 }//calculate intersection points Pi, Let, Pi = o + s*u, return value is s. hypothesis : o is out of the plane.
 
 int crossVect(double* pInter, double* u, double* p0, double* p1, double *normal) { //pInter and p0, p1 is in the same plane.
+	if(currRow==0&&currCol==146) cout<<"crossVect"<<endl;
+	if(currRow==0&&currCol==146) cout<<"p0 : "<<p0[0]<<", "<<p0[1]<<", "<<p0[2]<<endl;
+	if(currRow==0&&currCol==146) cout<<"p1 : "<<p1[0]<<", "<<p1[1]<<", "<<p1[2]<<endl;
 	double v[3];
 	for(int i = 0; i < 3; i++) v[i] = p1[i] - p0[i];
 	double d = length(v, 3);
+	if(currRow==0&&currCol==146) cout<<"d : "<<d<<endl;
 	for(int i = 0; i < 3; i++) v[i] = v[i]/d;
+	if(currRow==0&&currCol==146) cout<<"v : "<<v[0]<<", "<<v[1]<<", "<<v[2]<<endl;
 	double* N = crossProduct(v, normal); //new normal vector, and 
 	double len = length(N, 3);
 	for(int i = 0; i < 3; i++) N[i] = N[i]/len;
 	double D = calculD(p0, N);
+	if(currRow==0&&currCol==146) cout<<"N D :"<<N[0]<<", "<<N[1]<<", "<<N[2]<<", "<<D<<endl;
 	double s;
-	if(dotProduct(N, u) < 0.000000001 && dotProduct(N, u) > -0.00000001) { //parallel to plane
-		double temp = dotProduct(N, pInter) + D;
-		if(temp < 0.000001 && temp > -0.0000001) s = 0.0; //on the same plane.
-		else return 0;
+	if(dotProduct(N, u) < 0.001 && dotProduct(N, u) > -0.001) {
+		return 0;
+		/*double temp = dotProduct(N, pInter) + D;
+		if(temp == 0.0) s = 0.0;
+		//if(temp < 0.000001 && temp > -0.0000001) s = 0.0; //on the same plane.
+		else return 0;*/
 	} else {
 		s = -1.0*(D + dotProduct(N, pInter)) / dotProduct(N, u);
 	}
+	if(currRow==0&&currCol==146) cout<<"s : "<<s<<endl;
 	free(N);
 	if(s < 0.0) return 0; //no intersection. because cross point is behind the pInter.
 	double pNew[3];
 	for(int i = 0; i < 3; i++) pNew[i] = pInter[i] + u[i] * s; //cross Point.
+	if(currRow==0&&currCol==146) cout<<"pNew : "<<pNew[0]<<", "<<pNew[1]<<", "<<pNew[2]<<", "<<endl;
 	double vNew[3];
 	for(int i = 0; i < 3; i++) vNew[i] = pNew[i] - p0[i];
+	if(currRow==0&&currCol==146) cout<<"vNew : "<<vNew[0]<<", "<<vNew[1]<<", "<<vNew[2]<<", "<<endl;
 	if(dotProduct(vNew, v) < 0.0) return 0; //cross point is behind p0.
 	double dNew = length(vNew, 3);
+	if(currRow==0&&currCol==146) cout<<"dNdw : "<<dNew<<endl;
 	if(dNew < d) {
 		return 1;
 	}
@@ -512,11 +563,11 @@ int crossVect(double* pInter, double* u, double* p0, double* p1, double *normal)
 //and pInter+s*u, then the intersection point is on the vector v. because v is intersection line of N plane and normal plane.
 //finally, if length of p0 -> pNew is bigger than 0 and shorter than length(v), it has crosspoint.
 
-void setObject(string str) {
-	sphereNum = 3;
+void setMyObject() {
+	sphereNum = 0;
 	planeNum = 6;
 	lightNum = 1;
-	spheres = (Sphere*)malloc(sizeof(Sphere)*10);//replace 10 to sphereNum
+	spheres = (Sphere*)malloc(sizeof(Sphere)*256);//replace 10 to sphereNum
 	spheres[0].r = 30.0;
 	spheres[1].r = 30.0;
 	spheres[2].r = 30.0;
@@ -551,7 +602,7 @@ void setObject(string str) {
 
 	spheres[2].nr = 1.05;
 	//test part for sphere
-	planes = (Plane*)malloc(sizeof(Plane)*10);//replace 10 to planeNum
+	planes = (Plane*)malloc(sizeof(Plane)*256);//replace 10 to planeNum
 	for(int i = 0; i < 6; i++) {
 		planes[i].n = 4;
 		planes[i].vertex = (double **)malloc(sizeof(double*)*planes[i].n);
@@ -579,11 +630,6 @@ void setObject(string str) {
 	planes[3].vertex[1][0] = -350.0; planes[3].vertex[1][1] = -30.0; planes[3].vertex[1][2] = -350.0;
 	planes[3].vertex[2][0] = 350.0; planes[3].vertex[2][1] = -30.0; planes[3].vertex[2][2] = -350.0;
 	planes[3].vertex[3][0] = 350.0; planes[3].vertex[3][1] = 350.0; planes[3].vertex[3][2] = -350.0;
-
-	/*planes[4].vertex[0][0] = 350.0; planes[4].vertex[0][1] = 350.0; planes[4].vertex[0][2] = -350.0;
-	planes[4].vertex[1][0] = 350.0; planes[4].vertex[1][1] = 350.0; planes[4].vertex[1][2] = 350.0;
-	planes[4].vertex[2][0] = -350.0; planes[4].vertex[2][1] = 350.0; planes[4].vertex[2][2] = 350.0;
-	planes[4].vertex[3][0] = -350.0; planes[4].vertex[3][1] = 350.0; planes[4].vertex[3][2] = -350.0; */
 
 	planes[4].vertex[0][0] = 350.0; planes[4].vertex[0][1] = 350.0; planes[4].vertex[0][2] = -350.0;
 	planes[4].vertex[1][0] = 350.0; planes[4].vertex[1][1] = -30.0; planes[4].vertex[1][2] = -350.0;
@@ -617,7 +663,6 @@ void setObject(string str) {
 	planes[1].normal[0] = -1.0; planes[1].normal[1] = 0.0; planes[1].normal[2] = 1.0;
 	planes[2].normal[0] = 1.0; planes[2].normal[1] = 0.0; planes[2].normal[2] = 1.0;
 	planes[3].normal[0] = 0.0; planes[3].normal[1] = 0.0; planes[3].normal[2] = 1.0;
-	//planes[4].normal[0] = 0.0; planes[4].normal[1] = -1.0; planes[4].normal[2] = 0.0;
 	planes[4].normal[0] = -1.0; planes[4].normal[1] = 0.0; planes[4].normal[2] = 0.0;
 	planes[5].normal[0] = 1.0; planes[5].normal[1] = 0.0; planes[5].normal[2] = 0.0;
 
@@ -634,7 +679,6 @@ void setObject(string str) {
 	planes[3].shi = 80.0;
 	planes[4].shi = 80.0;
 	planes[5].shi = 80.0;
-	//planes[6].shi = 80.0;
 
 	for(int i = 0; i < 6; i++) {
 		planes[i].isTexture = 0;
@@ -697,6 +741,168 @@ void setTexture(string str, int a, int b, int c, int d) {//set the texture of th
 	return;
 }
 
+void objParser(string str, double* trans, double scale) {
+	ifstream inputFile(str.data());
+	if(inputFile.fail()) {
+		cout<<"The object file of the name doesn't exist!"<<endl;
+		return;
+	}
+	int check = 0; //vt checker
+	double amb[] = {0.0, 0.0, 0.0};
+	double dif[] = {0.1, 0.1, 0.7};
+	double spe[] = {0.0, 0.0, 0.0};
+	double shi = 98.0;
+	//After test, the vectros changes to pointer, and the value is in the mtl file.
+	vertexNum = 0;
+	normalNum = 0;
+	vertexsSize = 256;
+	normalsSize = 256;
+	vertexs = (double**)malloc(sizeof(double*)*vertexsSize);
+	normals = (double**)malloc(sizeof(double*)*normalsSize);
+	string line;
+	string mtlName;
+	while(getline(inputFile, line)) { //read line by line
+		string temp = line; //deep Copy
+		temp.erase(remove(temp.begin(), temp.end(), ' '), temp.end()); //space removing
+		temp.erase(remove(temp.begin(), temp.end(), '\t'), temp.end()); //tab removing
+		if(temp.length() == 0) continue; //blank line
+		if(temp.at(0)=='#') continue; //remark line
+		if(temp.at(0)=='o') continue;
+		if(temp.at(0)=='s') continue;
+		//meaningful line part start
+		size_t found = line.find_first_of(' ');
+		string tag = line.substr(0, found);
+		//cout<<tag<<endl;
+		if(tag.compare("mtllib")==0) {//mtllb tag
+			line = line.substr(found+1, line.length()); //tag remove
+			found = line.find_first_of('#');
+			if(found == string::npos) found = line.length();
+			mtlName = line.substr(0, found); //save mtl file name to mtlName
+			/*
+				part for extract the information from the mtl file.
+			*/
+		} else if(tag.compare("v")==0) {//vertex tag
+			line = line.substr(found+1, line.length()); //tag remove
+			found = line.find_first_of('#'); //remark remover
+			if(found == string::npos) found = line.length();
+			line = line.substr(0, found);
+			if(vertexNum == vertexsSize) {//realloc
+				vertexsSize = 2*vertexsSize;
+				double** tempVertexs = (double**)realloc(vertexs, sizeof(double*)*vertexsSize);
+				vertexs = tempVertexs;
+			}
+			vertexs[vertexNum] = (double *)malloc(sizeof(double)*3); //allocation
+			for(int i = 0; i < 3; i++) { //allocaton
+				found = line.find_first_of(' ');
+				if(found == string::npos) found = line.length();
+				vertexs[vertexNum][i] = stod(line.substr(0, found))*scale + trans[i];
+				if(found!=line.length()) {
+					line = line.substr(found+1, line.length());
+				}
+			}
+			vertexNum++;
+		} else if(tag.compare("vn")==0) {
+			line = line.substr(found+1, line.length());
+			found = line.find_first_of('#');
+			if(found == string::npos) found = line.length();
+			line = line.substr(0, found);
+			if(normalNum == normalsSize) {
+				normalsSize = 2*normalsSize;
+				double** tempNormals = (double**)realloc(normals, sizeof(double*)*normalsSize);
+				normals = tempNormals;
+			}
+			normals[normalNum] = (double*)malloc(sizeof(double)*3);
+			for(int i = 0; i < 3; i++) {
+				found = line.find_first_of(' ');
+				if(found == string::npos) found = line.length();
+				normals[normalNum][i] = stod(line.substr(0, found));
+				if(found!=line.length()) {
+					line = line.substr(found+1, line.length());
+				}
+			}
+			normalNum++;
+		} else if(tag.compare("usemtl")==0) {//set material
+		} else if(tag.compare("vt")==0) {
+			check = 1;
+		} else if(tag.compare("f")==0) {//part of face
+			line = line.substr(found+1, line.length());
+			found = line.find_first_of('#');
+			if(found == string::npos) found = line.length();
+			line = line.substr(0, found);
+			if(planeNum == planesSize) {
+				planesSize = 2*planesSize;
+				Plane* tempPlanes = (Plane*)realloc(planes, sizeof(Plane)*planesSize);
+				planes = tempPlanes;
+			}
+			//some information updated.
+			planes[planeNum].shi = shi;
+			planes[planeNum].alpha = 1.0;
+			planes[planeNum].isTexture = 0;
+			for(int i = 0; i < 3; i++) {
+				planes[planeNum].dif[i] = dif[i];
+				planes[planeNum].spe[i] = spe[i];
+				planes[planeNum].amb[i] = amb[i];
+			}
+			int normalIdx = -1; //normal vector idx
+			int tempVertex[30]; //vertex idx array
+			int n = 0; //number of vertex in the plane
+			for(int i = 0; i < 30; i++) {
+				//cout<<i<<endl;
+				//cout<<line<<endl;
+				found = line.find_first_of(' ');
+				if(found == string::npos) found = line.length();
+				if(check == 0) {//'v//vn' form
+					string chunk = line.substr(0, found); //'v//vn'
+					size_t tempFound = chunk.find_first_of('/');
+					tempVertex[n] = stoi(chunk.substr(0, tempFound))-1;
+					int tempNormal = stoi(chunk.substr(tempFound+2, chunk.length()))-1;
+					if(normalIdx == -1) {//first time
+						normalIdx = tempNormal;
+					} else if(normalIdx != tempNormal) {
+						cout<<"normal vector of objParser isn't same"<<endl;
+						return;
+					}
+					n++;
+					if(found!=line.length()) {
+						line = line.substr(found+1, line.length());
+						if(i==29) {
+							cout<<"plane vertex has more than 30"<<endl;
+							return;
+						}
+					} else break; //all vertex inserted
+				} else {//'v/vt/vn' form
+				}
+			}
+			//cout<<n<<endl;
+			planes[planeNum].n = n;
+			planes[planeNum].vertex = (double**)malloc(sizeof(double*)*n);
+			for(int i = 0; i < n; i++) {
+				//cout<<"i2: "<<i<<endl;
+				planes[planeNum].vertex[i] = (double*)malloc(sizeof(double)*3);
+				for(int j = 0; j < 3; j++) {
+					planes[planeNum].vertex[i][j] = vertexs[tempVertex[i]][j];
+				}
+			}
+			for(int i = 0; i < 3; i++) planes[planeNum].normal[i] = normals[normalIdx][i];
+			planes[planeNum].D = calculD(planes[planeNum].vertex[0], planes[planeNum].normal);
+			planeNum++;
+		}
+	}
+	cout<<vertexNum<<endl;
+	cout<<normalNum<<endl;
+	cout<<planeNum<<endl;
+	for(int i = 0; i < normalNum; i++) {
+		free(normals[i]);
+	}
+	free(normals);
+	for(int i = 0; i < vertexNum; i++) {
+		free(vertexs[i]);
+	}
+	free(vertexs);
+	return;
+	/*Sphere* tempSpheres = (Sphere*)realloc(spheres, sizeof(Sphere) * (2 * spheresSize));
+	spheres = tempSpheres;*/ //realloc example
+}
 double calculD(double* vertex, double* normal) {
 	return -1.0 * dotProduct(vertex, normal);
 }
